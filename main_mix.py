43ff23025c7cfbac7d229c348f2959836a913ea6
Aritra <aritrabal2@gmail.com>
2023-03-02 12:24:29 +0100

    Current jet mixing

--- main_mix.py
+++ main_mix.py
@@ -11,11 +11,19 @@ import case_paths.util.sample_factory as sf
 import case_paths.path_constants.sample_dict_file_parts_reco as sdfr
 import case_analysis.util.sample_names as samp
 
-def create_libraries(binning):
+def normalize(df,max=None):
+    if max:
+        return df/max
+    return (df-df.min())/(df.max()-df.min())
 
+def create_libraries(binning):
+    reg = 250
+    
     branches = ['mJJ', 'DeltaEtaJJ', 'j1Pt', 'j1Eta', 'j1Phi', 'j1M',
                 'j2Pt', 'j2Eta', 'j2Phi', 'j2M', 'j1RecoLoss', 'j1KlLoss',
                 'j2RecoLoss', 'j2KlLoss']
+    
+    k_NN=5
     
     run_n=50005    
     signals=samp.all_samples
@@ -27,8 +35,9 @@ def create_libraries(binning):
     mixed_id='qcdSigMCOrigReco_mixedJets'
     paths = sf.SamplePathDirFactory(sdfr.path_dict).update_base_path({'$run$': 'run_'+str(run_n)})
 
+    signals = ['WkkToWRadionToWWW_M3000_Mr170Reco']
+    injected=[0.1]
     
-
     for sig_sample_id in signals:
         print('###############################')
         print(sig_sample_id)
@@ -37,18 +46,29 @@ def create_libraries(binning):
         for inj in injected:    
         
             libs_mjj = {}
+            
             libs_j2pt = {}
             libs_j2eta = {}
+            libs_j2phi = {}
             libs_j2reco = {}
             libs_j2kl = {}
+            
+            libs_j1pt = {}
+            libs_j1eta = {}
+            libs_j1phi = {}
+            libs_j1reco = {}
+            libs_j1kl = {}
+            
             evts_mjj = {}
             evts_deta = {}
             evts_j1pt = {}
             evts_j1eta = {}
+            evts_j1phi = {}
             evts_j1reco = {}
             evts_j1kl = {}
             evts_j2pt = {}
             evts_j2eta = {}
+            evts_j2phi = {}
             evts_j2reco = {}
             evts_j2kl = {}
 
@@ -56,27 +76,44 @@ def create_libraries(binning):
             idxs = {}
 
             kdtrees = {}
-            
+            kdtrees1 = {}
             #for i,filename in enumerate(glob.glob(path+"/*h5")):
             
             
             
             filename=paths.sample_file_path(injected_id,additional_id=sig_sample_id,overwrite=True,customname=f'data_{sig_sample_id}_{inj}')
+            
+            #filename=f'/storage/9/abal/CASE/VAE_results/events/run_{run_n}/qcd_sig_orig_RECO_injected_jets/sig_MCOrig_QR_Reco.h5'
+            
             print(f'Performing mixing on {filename}')
             with h5py.File(filename, "r") as f:
                 features = f['eventFeatures'][()]
+                
                 # Get the indices of the featureNames when opening the first file
                 if len(idxs) == 0:
                     featureNames = f['eventFeatureNames'][()].astype(str)
                     for b in branches:
-                        idxs[b] = np.argwhere(featureNames == b)[0][0]
+                        try:
+                            idxs[b] = np.argwhere(featureNames == b)[0][0]
+                        except NameError:
+                            print(f"Column {b} probably doesn't exist in this dataset.")
+                            pass
+                features[:,idxs['j1Pt']]=normalize(features[:,idxs['j1Pt']])
+                features[:,idxs['j1Eta']]=normalize(features[:,idxs['j1Eta']],max=2.5)
+                features[:,idxs['j1Phi']]=normalize(features[:,idxs['j1Phi']],max=3.15)
+                
+                features[:,idxs['j2Pt']]=normalize(features[:,idxs['j2Pt']])
+                features[:,idxs['j2Eta']]=normalize(features[:,idxs['j2Eta']],max=2.5)
+                features[:,idxs['j2Phi']]=normalize(features[:,idxs['j2Phi']],max=3.15)
+                
                         
                 for b,lower in enumerate(binning): 
                     if b ==	len(binning)-1:
                         upper = 1e9
                     else:
                         upper = binning[b+1]
-
+                    
+                    #pdb.set_trace()
                     # Sorting events according to the bins
                     tmp_features = features[ (features[:,idxs['mJJ']] > lower ) & (features[:,idxs['mJJ']] <= upper )]
 
@@ -86,38 +123,56 @@ def create_libraries(binning):
                     if b not in evts_mjj:
                         evts_mjj[b] = tmp_features[:,idxs['mJJ']]
                         evts_deta[b] = tmp_features[:,idxs['DeltaEtaJJ']]
-                        evts_j1pt[b] = np.log(tmp_features[:,idxs['j1Pt']])
+                        evts_j1pt[b] = (tmp_features[:,idxs['j1Pt']]) # WARNING: NP.LOG IF NOT NORMALIZED
                         evts_j1eta[b] = tmp_features[:,idxs['j1Eta']]
+                        evts_j1phi[b] = tmp_features[:,idxs['j1Phi']]
                         evts_j1reco[b] = tmp_features[:,idxs['j1RecoLoss']]
                         evts_j1kl[b] = tmp_features[:,idxs['j1KlLoss']]
-                        evts_j2pt[b] = np.log(tmp_features[:,idxs['j2Pt']])
+                        evts_j2pt[b] = (tmp_features[:,idxs['j2Pt']]) # WARNING: NP.LOG IF NOT NORMALIZED
                         evts_j2eta[b] = tmp_features[:,idxs['j2Eta']]
+                        evts_j2phi[b] = tmp_features[:,idxs['j2Phi']]
                         evts_j2reco[b] = tmp_features[:,idxs['j2RecoLoss']]
                         evts_j2kl[b] = tmp_features[:,idxs['j2KlLoss']]
                     else:
                         evts_mjj[b] = np.concatenate((evts_mjj[b],tmp_features[:,idxs['mJJ']]),axis=0)
                         evts_deta[b] = np.concatenate((evts_deta[b],tmp_features[:,idxs['DeltaEtaJJ']]),axis=0)
-                        evts_j1pt[b] = np.concatenate((evts_j1pt[b],np.log(tmp_features[:,idxs['j1Pt']])),axis=0)
+                        evts_j1pt[b] = np.concatenate((evts_j1pt[b],(tmp_features[:,idxs['j1Pt']])),axis=0) # WARNING: NP.LOG IF NOT NORMALIZED
                         evts_j1eta[b] = np.concatenate((evts_j1eta[b],tmp_features[:,idxs['j1Eta']]),axis=0)
+                        evts_j1phi[b] = np.concatenate((evts_j1phi[b],tmp_features[:,idxs['j1Phi']]),axis=0)
                         evts_j1reco[b] = np.concatenate((evts_j1reco[b],tmp_features[:,idxs['j1RecoLoss']]),axis=0)
                         evts_j1kl[b] = np.concatenate((evts_j1kl[b],tmp_features[:,idxs['j1KlLoss']]),axis=0)
-                        evts_j2pt[b] = np.concatenate((evts_j2pt[b],np.log(tmp_features[:,idxs['j2Pt']])),axis=0)
-                        evts_j2eta[b] = np.concatenate((evts_j2eta[b],tmp_features[:,idxs['j2Eta']]),axis=0)
+                        evts_j2pt[b] = np.concatenate((evts_j2pt[b],(tmp_features[:,idxs['j2Pt']])),axis=0)
+                        evts_j2eta[b] = np.concatenate((evts_j2eta[b],tmp_features[:,idxs['j2Eta']]),axis=0) # WARNING: NP.LOG IF NOT NORMALIZED
+                        evts_j2phi[b] = np.concatenate((evts_j2phi[b],tmp_features[:,idxs['j2Phi']]),axis=0)
                         evts_j2reco[b] = np.concatenate((evts_j2reco[b],tmp_features[:,idxs['j2RecoLoss']]),axis=0)
                         evts_j2kl[b] = np.concatenate((evts_j2kl[b],tmp_features[:,idxs['j2KlLoss']]),axis=0)
-                    #pdb.set_trace()
                     # Creating a library per bin that corresponds to the inverse
                     tmp_features = features[ (features[:,idxs['mJJ']] <= lower ) | (features[:,idxs['mJJ']] > upper )]
                     if b not in libs_mjj:
-                        libs_j2pt[b] = np.log(tmp_features[:,idxs['j2Pt']])
+                        libs_j2pt[b] = (tmp_features[:,idxs['j2Pt']]) # WARNING: NP.LOG IF NOT NORMALIZED
                         libs_j2eta[b] = tmp_features[:,idxs['j2Eta']]
+                        libs_j2phi[b] = tmp_features[:,idxs['j2Phi']]
                         libs_j2reco[b] = tmp_features[:,idxs['j2RecoLoss']]
                         libs_j2kl[b] = tmp_features[:,idxs['j2KlLoss']]
+                        
+                        libs_j1pt[b] = (tmp_features[:,idxs['j1Pt']]) # WARNING: NP.LOG IF NOT NORMALIZED
+                        libs_j1eta[b] = tmp_features[:,idxs['j1Eta']]
+                        libs_j1phi[b] = tmp_features[:,idxs['j1Phi']]
+                        libs_j1reco[b] = tmp_features[:,idxs['j1RecoLoss']]
+                        libs_j1kl[b] = tmp_features[:,idxs['j1KlLoss']]
+                    
                     else:
-                        libs_j2pt[b] = np.concatenate((libs_j2pt[b],np.log(tmp_features[:,idxs['j2Pt']])),axis=0)
+                        libs_j2pt[b] = np.concatenate((libs_j2pt[b],(tmp_features[:,idxs['j2Pt']])),axis=0) # WARNING: NP.LOG IF NOT NORMALIZED
                         libs_j2eta[b] = np.concatenate((libs_j2eta[b],tmp_features[:,idxs['j2Eta']]),axis=0)
+                        libs_j2phi[b] = np.concatenate((libs_j2phi[b],tmp_features[:,idxs['j2Phi']]),axis=0)
                         libs_j2reco[b] = np.concatenate((libs_j2reco[b],tmp_features[:,idxs['j2RecoLoss']]),axis=0)
                         libs_j2kl[b] = np.concatenate((libs_j2kl[b],tmp_features[:,idxs['j2KlLoss']]),axis=0)
+                        
+                        libs_j1pt[b] = np.concatenate((libs_j1pt[b],(tmp_features[:,idxs['j1Pt']])),axis=0) # WARNING: NP.LOG IF NOT NORMALIZED
+                        libs_j1eta[b] = np.concatenate((libs_j1eta[b],tmp_features[:,idxs['j1Eta']]),axis=0)
+                        libs_j1phi[b] = np.concatenate((libs_j1phi[b],tmp_features[:,idxs['j1Phi']]),axis=0)
+                        libs_j1reco[b] = np.concatenate((libs_j1reco[b],tmp_features[:,idxs['j1RecoLoss']]),axis=0)
+                        libs_j1kl[b] = np.concatenate((libs_j1kl[b],tmp_features[:,idxs['j1KlLoss']]),axis=0)
 
 
 
@@ -130,13 +185,19 @@ def create_libraries(binning):
             for b,lower in enumerate(binning):
                 #if b > 1:
                 #    break
-
                 data_for_kdtree = np.stack((libs_j2pt[b],libs_j2eta[b]),axis=-1)
                 data_in_bin = np.stack((evts_j2pt[b],evts_j2eta[b]),axis=-1)
 
                 kdtrees[b] = KDTree(data_for_kdtree)        
                 dist,ind = kdtrees[b].query(data_in_bin, k=1)
                 flat_ind = ind.flatten()
+
+                data_for_kdtree1 = np.stack((libs_j1pt[b],libs_j1eta[b]),axis=-1)
+                data_in_bin1 = np.stack((evts_j1pt[b],evts_j1eta[b]),axis=-1)
+                
+                kdtrees1[b] = KDTree(data_for_kdtree1)        
+                dist1,ind1 = kdtrees1[b].query(data_in_bin1, k=1)
+                flat_ind1 = ind1.flatten()
 
                 #print("Data in bin",b)
                 #print(data_in_bin.shape)
@@ -151,14 +212,19 @@ def create_libraries(binning):
                 evts_j1kl[b] = np.expand_dims(evts_j1kl[b],axis=1)
                 evts_j2pt[b] = np.expand_dims(np.exp(evts_j2pt[b]),axis=1)
                 evts_j2eta[b] = np.expand_dims(evts_j2eta[b],axis=1)
+                
+                pdb.set_trace()
                 replaced_j2reco = np.expand_dims(libs_j2reco[b][flat_ind],axis=1)
                 replaced_j2kl = np.expand_dims(libs_j2kl[b][flat_ind],axis=1)
-
+                
+                replaced_j1reco = np.expand_dims(libs_j1reco[b][flat_ind1],axis=1)
+                replaced_j1kl = np.expand_dims(libs_j1kl[b][flat_ind1],axis=1)
+                
                 #print(replaced_j2reco.shape)
                 #print(replaced_j2kl.shape)
                 #print(evts_j2pt[b].shape)
                 
-                final_features_in_bin = np.squeeze(np.stack((evts_mjj[b],evts_deta[b],evts_j1pt[b],evts_j1eta[b],evts_j1reco[b],evts_j1kl[b],\
+                final_features_in_bin = np.squeeze(np.stack((evts_mjj[b],evts_deta[b],evts_j1pt[b],evts_j1eta[b],replaced_j1reco,replaced_j1kl,\
                                                             evts_j2pt[b],evts_j2eta[b],replaced_j2reco,replaced_j2kl),axis=-1),axis=1)
                 #print(final_features_in_bin)
                 #print(final_features_in_bin.shape)
@@ -169,8 +235,8 @@ def create_libraries(binning):
                     final_arr = np.concatenate((final_arr,final_features_in_bin),axis=0)
 
             #print(final_arr.shape)
-            out_filename=paths.sample_file_path(mixed_id,additional_id=sig_sample_id,mkdir=True,overwrite=True,customname=f'data_MIXED_{sig_sample_id}_{inj}')
-            
+            out_filename=paths.sample_file_path(mixed_id,additional_id=sig_sample_id,mkdir=True,overwrite=True,customname=f'data_MIXED_{sig_sample_id}_{inj}_normalized')
+            #out_filename = f'/storage/9/abal/CASE/VAE_results/events/run_{run_n}/qcd_sig_orig_RECO_injected_jets/sig_MCOrig_QR_Reco_MIXED.h5'
             print("Complete. Now creating dataset")
             print(f"Output file: {out_filename}")
             
@@ -199,7 +265,7 @@ if __name__ == '__main__':
                2111, 2206, 2305, 2406, 2512, 2620, 2733, 2849, 2969, 3093,
                3221, 3353, 3490, 3632, 3778, 3928, 4084, 4245, 4411, 4583,
                4760, 4943, 5132, 5327, 5527]
-
+    
     #binning = [1450, 2450, 3450, 4450, 5450]
 
     create_libraries(binning)
